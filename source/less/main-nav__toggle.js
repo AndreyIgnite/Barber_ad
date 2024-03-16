75b4050bb92bcb44fa960665c08b1b789a0f3305
Andrey <mozobretenie@gmail.com>
2024-03-16 09:48:33 +0300

    w

--- source/less/main-nav__toggle.js
+++
@@ -1,6 +0,0 @@
-let toogler = document.querySelector(".main-nav__toggle");
-let navigation = document.querySelector(".main-nav");
-toogler.addEventListener("click", function () {
-  navigation.classList.toggle('main-nav--opened')
-  navigation.classList.toggle('main-nav--closed')
-});
