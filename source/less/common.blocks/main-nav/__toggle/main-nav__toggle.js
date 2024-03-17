b748010af4f1a85edab4a888ac499416756a9b41
Andrey <mozobretenie@gmail.com>
2024-03-17 23:16:59 +0300

    w

--- source/less/common.blocks/main-nav/__toggle/main-nav__toggle.js
+++ source/less/common.blocks/main-nav/__toggle/main-nav__toggle.js
@@ -1,5 +1,7 @@
+let nojs = document.querySelector(".main-nav--nojs")
 let toogler = document.querySelector(".main-nav__toggle");
 let navigation = document.querySelector(".main-nav");
+nojs.classList.remove("main-nav--nojs")
 toogler.addEventListener("click", function () {
   navigation.classList.toggle('main-nav--opened')
   navigation.classList.toggle('main-nav--closed')
--- source/main-nav__toggle.js
+++ source/main-nav__toggle.js
@@ -1,6 +1,8 @@
+let nojs = document.querySelector(".main-nav--nojs");
 let toogler = document.querySelector(".main-nav__toggle");
 let navigation = document.querySelector(".main-nav");
+nojs.classList.remove("main-nav--nojs");
 toogler.addEventListener("click", function () {
-  navigation.classList.toggle('main-nav--opened')
-  navigation.classList.toggle('main-nav--closed')
+  navigation.classList.toggle("main-nav--opened");
+  navigation.classList.toggle("main-nav--closed");
 });
